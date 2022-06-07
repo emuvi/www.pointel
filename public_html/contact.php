9fc226564c524fbd273532c48eebac75d79267c0
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-07 13:45:23 -0300

    sets the headers to send the mail

--- public_html/contact.php
+++ public_html/contact.php
@@ -1,6 +1,11 @@
 <?php
-$envio = mail("emuvi@pointel.com.br", "Contact from Website", "This is a test message.");
-if ($envio) {
+$sender = "mail@pointel.com.br";
+$headers = "MIME-Version: 1.1\n";
+$headers .= "Content-type: text/plain; charset=UTF-8\n";
+$headers .= "From: $sender\n";
+$headers .= "Return-Path: $sender\n";
+$send = mail("emuvi@pointel.com.br", "Contact from Website", "This is a test message.", $headers, "-f$sender");
+if ($send) {
     echo "Mensagem enviada com sucesso!";
 } else {
     echo "Erro ao enviar mensagem!";
