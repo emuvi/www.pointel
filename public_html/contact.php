10be4c9643319fd2b5c01bc25aebd26b4e2b0542
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-07 13:56:05 -0300

    sets the message body

--- public_html/contact.php
+++ public_html/contact.php
@@ -4,7 +4,14 @@ $headers = "MIME-Version: 1.1\n";
 $headers .= "Content-type: text/plain; charset=UTF-8\n";
 $headers .= "From: $sender\n";
 $headers .= "Return-Path: $sender\n";
-$send = mail("emuvi@pointel.com.br", "Contact from Website", "This is a test message.", $headers, "-f$sender");
+$name = $_POST['name'];
+$fone = $_POST['fone'];
+$mail = $_POST['mail'];
+$message = "From website:\n";
+$message .= "Nome: $name\n";
+$message .= "Telefone: $fone\n";
+$message .= "E-mail: $mail\n";
+$send = mail("emuvi@pointel.com.br", "Contact from Website", $message, $headers, "-f$sender");
 if ($send) {
     echo "Mensagem enviada com sucesso!";
 } else {
